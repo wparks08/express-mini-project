64e4fb1785192375ba0c2830b4793cc027acd484
wparks08 <wparks08@gmail.com>
2019-12-14 11:10:28 -0800

    adjust html routes to serve pages. add api routes

--- index.js
+++ index.js
@@ -12,23 +12,32 @@ app.use(express.json());
 //GET home
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
-})
+});
 
 //GET reservations
-app.get("/reserve", function (req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-})
+app.get("/new.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "new.html"));
+});
 
 //GET viewTables
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
-})
+});
 
 //GET reservations [JSON]
+app.get("/api/reservations", function (req, res) {
+    return res.json({ debug: "in process" })
+})
 
 //GET waitlist [JSON]
+app.get("/api/waitlist", function (req, res) {
+    return res.json({ debug: "in process" })
+})
 
 //POST makeReservation
+app.post("/api/makeReservation", function (req, res) {
+
+})
 
 app.listen(PORT, function () {
     console.log("Server listening on port " + PORT);
